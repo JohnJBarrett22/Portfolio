98822ac09d5e0df5750a3b75e17aaecda4829271
John Barrett <johnjbarrett22@gmail.com>
2019-05-04 11:50:05 -0700

    Added sticky nav, though need to adjust it to start below video into, working on footer icon links to social media sites

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';  
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[ 
+    trigger('fade',
+    [ 
+      state('void', style({ opacity : 0})),
+      transition(':enter',[ animate(300)]),
+      transition(':leave',[ animate(500)]),
+    ]
+)]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
-  
+  constructor(@Inject(DOCUMENT) document) { }
+
+  ngOnInit() {  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e) {
+     if (window.pageYOffset > 550) {
+       let element = document.getElementById('navbar');
+       element.classList.add('sticky');
+     } else {
+      let element = document.getElementById('navbar');
+        element.classList.remove('sticky'); 
+     }
+  }
 }
