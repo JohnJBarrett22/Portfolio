526404dd5d7598b33f1f22ea55e903571048cf4d
John Barrett <johnjbarrett22@gmail.com>
2019-05-30 15:39:05 -0700

    Figured out animation syntax finally, going to flesh out through the site

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -79,10 +79,27 @@ export class AppComponent implements OnInit {
   ngAfterViewInit(){
     var navi = document.getElementById('myNav');
     this.navPosition = navi.offsetTop;
-    var wow = new WOW({
-      live: false
+
+    var wow = new WOW(
+      {
+        boxClass:     'wow',      // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset:       300,          // distance to the element when triggering the animation (default is 0)
+        mobile:       true,       // trigger animations on mobile devices (default is true)
+        live:         false,       // act on asynchronously loaded content (default is true)
+        callback:     function(box) {
+          // the callback is fired every time an animation is started
+          // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null // optional scroll container selector, otherwise use window
+      }
+    );
+    wow.init();
+
+    document.addEventListener("DOMContentLoaded", function() { 
+      wow.sync()
+      console.log("Sync");
     });
-    wow.init()
     
   }
 
